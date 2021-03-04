cbef66fc5079834ab157fff353cb5626e8dbbe89
ZakiAfifi <80030239+ZakiAfifi@users.noreply.github.com>
2021-03-04 16:01:28 +0700

    Membuat program sederhana untuk menentukan angka
    angka positif dan negatif

---
+++ PI.js
@@ -0,0 +1,8 @@
+alert('Program Sederhana Angka Positif dan Negatif');
+let angka = prompt('Masukan Angka');
+
+if (angka >= 0) {
+  alert('Ini Angka Positif');
+} else {
+  alert('Ini Angka Negatif');
+}
