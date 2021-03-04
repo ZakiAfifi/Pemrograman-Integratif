99b7474f59bb791996db0f68abe1a6a639c75b78
ZakiAfifi <80030239+ZakiAfifi@users.noreply.github.com>
2021-03-04 16:02:43 +0700

    Mengubah sesuai permintaan client

--- PI.js
+++ PI.js
@@ -1,8 +1,12 @@
-alert('Program Sederhana Angka Positif dan Negatif');
-let angka = prompt('Masukan Angka');
+alert('Program Sederhana Angka Positif dan Negatif')
+  let angka = parseInt(prompt('Masukan Angka'))
 
-if (angka >= 0) {
-  alert('Ini Angka Positif');
-} else {
-  alert('Ini Angka Negatif');
-}
+  if (angka >= 0) {
+    alert('Ini Angka Positif');
+  } else if( angka == 0){
+    alert('Ini Angka 0');
+  } else if (angka < 0) {
+    alert('Ini Angka Negatif');
+  } else {
+    alert('Ini Bukan Angka');
+  }
